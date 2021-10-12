e4066fd62ad7b1803210e18634b040520aeea04c
Fasil <fasil17062002@gmail.com>
2021-10-12 12:34:06 +0530

    share preference cursor bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -9,110 +9,108 @@ void main() async {
   var email = prefs.getString('email');
   print(email);
   runApp(
-    email == null ? LoginPage() : HomePage(),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: email == null ? LoginPage() : HomePage(),
+    ),
   );
 }
 
 class LoginPage extends StatelessWidget {
-  TextEditingController controller = new TextEditingController();
   GlobalKey<FormState> formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Builder(
-        builder: (context) => Scaffold(
-          body: Form(
-            key: formkey,
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.only(top: 150),
-                    child: Text(
-                      'Sign in',
-                      style: GoogleFonts.crimsonPro(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 35,
-                      ),
+    return Builder(
+      builder: (context) => Scaffold(
+        body: Form(
+          key: formkey,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.only(top: 150),
+                  child: Text(
+                    'Sign in',
+                    style: GoogleFonts.crimsonPro(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 35,
                     ),
                   ),
-                  Container(
-                    padding: EdgeInsets.only(
-                      left: 30,
-                      right: 30,
-                      bottom: 20,
-                      top: 30,
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                    left: 30,
+                    right: 30,
+                    bottom: 20,
+                    top: 30,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
                     ),
-                    child: TextFormField(
-                      controller: controller,
+                    validator: (value) {
+                      if (value == "") {
+                        return "Username reqiured";
+                      } else if (value != "f") {
+                        return "Incorrect username!!";
+                      }
+                      //fasil
+                    },
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                    left: 30,
+                    right: 30,
+                  ),
+                  child: TextFormField(
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText: 'Username',
+                        labelText: 'Password',
                       ),
                       validator: (value) {
                         if (value == "") {
-                          return "Username reqiured";
-                        } else if (value != "f") {
-                          return "Incorrect username!!";
+                          return "Password reqiured";
+                        } else if (value != "1") {
+                          return "Incorrect Password!!";
                         }
-                        //fasil
-                      },
-                    ),
+                        //123456
+                      }),
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                    left: 150,
+                    right: 150,
+                    top: 20,
+                    bottom: 15,
                   ),
-                  Container(
-                    padding: EdgeInsets.only(
-                      left: 30,
-                      right: 30,
-                    ),
-                    child: TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Password',
-                        ),
-                        validator: (value) {
-                          if (value == "") {
-                            return "Password reqiured";
-                          } else if (value != "1") {
-                            return "Incorrect Password!!";
-                          }
-                          //123456
-                        }),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(
-                      left: 150,
-                      right: 150,
-                      top: 20,
-                      bottom: 15,
-                    ),
-                    child: ElevatedButton(
-                      child: Text(
-                        "Log in",
-                        style: GoogleFonts.saira(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 15,
-                        ),
+                  child: ElevatedButton(
+                    child: Text(
+                      "Log in",
+                      style: GoogleFonts.saira(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
                       ),
-                      onPressed: () async {
-                        if (formkey.currentState!.validate()) {
-                          SharedPreferences prefs =
-                              await SharedPreferences.getInstance();
-                          prefs.setString('email', 'username');
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (BuildContext ctx) => HomePage(),
-                            ),
-                          );
-                        }
-                      },
                     ),
+                    onPressed: () async {
+                      if (formkey.currentState!.validate()) {
+                        SharedPreferences prefs =
+                            await SharedPreferences.getInstance();
+                        prefs.setString('email', 'username');
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext ctx) => HomePage(),
+                          ),
+                        );
+                      }
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
